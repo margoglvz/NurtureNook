4e88b5099c0de2d8b4b704d06c0178847700f38b
margoglvz <msgalvez06@gmail.com>
2024-01-28 10:27:53 -0800

    sage

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
 from ParksYelpAPI import getParks_byCity
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/<city>")
 def get_API(city):
